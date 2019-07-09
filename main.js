23bdcb6ccdfc002ddbda96275eb8e54fcaf044f2
AndrewLi <13058100289@163.com>
2019-07-09 23:12:06 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+/*
+
+  given I have an array of barcode like ['0001', '0003', '0005', '0003']
+      and exist an databases about goods like
+          {"id": "0001", "name": "Coca Cola", "price": 3},
+          {"id": "0002", "name": "Diet Coke", "price": 4},
+          {"id": "0003", "name": "Pepsi-Cola", "price": 5},
+          {"id": "0004", "name": "Mountain Dew", "price": 6},
+          {"id": "0005", "name": "Dr Pepper", "price": 7},
+          {"id": "0006", "name": "Sprite", "price": 8},
+          {"id": "0007", "name": "Diet Pepsi", "price": 9},
+          {"id": "0008", "name": "Diet Mountain Dew", "price": 10},
+          {"id": "0009", "name": "Diet Dr Pepper", "price": 11},
+          {"id": "0010", "name": "Fanta", "price": 12}
+
+  when I input the barCode List below : ['0001', '0003', '0005', '0003']
+  Then I should get the Receipt about detail info of goods such as:
+          Receipts
+          ------------------------------------------------------------
+          Coca Cola  3  1
+          Pepsi-Cola  5  2
+          Dr Pepper  7  1
+          ------------------------------------------------------------
+          Price: 20
+
+ */
+
+
+
+
 
 /*
   barCode: ['0001','0003', '0005', '0003']
